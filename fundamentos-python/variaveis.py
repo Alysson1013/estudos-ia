da12cec86f79144325e74f2c8336e78b5043cd8e
Alysson Eduardo <alysson6801@hotmail.com>
2021-01-11 23:08:52 -0300

    (11/01) Variáveis em Python

---
+++ fundamentos-python/variaveis.py
@@ -0,0 +1,27 @@
+x = 10
+y = 20
+z = 100
+w = (10 * 20) + 100
+print(w)
+
+print("Texto impresso")
+print("Texto e duas variaveis", x , ",", y)
+
+m = "Python"
+print(type(type(m)))
+
+
+print("Informe um valor: ")
+i = input()
+print(type(i))
+
+var = input("Informe o Valor: ")
+
+w = (w**2) + (z*x*y) + float(var)
+
+#Exemplo de comentário em Python
+'''Comentário de multiplas linhas em 
+Python'''
+
+print(w)
+print(type(var))
