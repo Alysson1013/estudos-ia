d6445d5542efd76781e2643106e9fd53e3d9a64d
Alysson Eduardo <alysson6801@hotmail.com>
2021-01-12 21:28:41 -0300

    (12/01) Listas

---
+++ fundamentos-python/listas.py
@@ -0,0 +1,14 @@
+vet = [1,2,3,4,5]
+print(vet)
+
+vet2 = [1,"dois", True]
+print(vet2)
+
+vet3 = [12, [1,2,3,4,5,6], False, "Carlos"]
+print(vet3)
+print(vet3[1][4])
+
+print(len(vet3))
+
+for n in vet3:
+    print(n)
